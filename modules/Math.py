1f3f96feffa2554b73e4a911ed4368839d89ecad
buble-security <sensegamer4@gmail.com>
2026-02-27 23:12:20 +0700

    test uploaded module python

---
+++ modules/Math.py
@@ -0,0 +1,22 @@
+import math 
+
+# pangkat 
+print(math.pow(2, 3)) # jika ingin sederhana gunakan module ini, untuk perpangkatan 
+
+# mencari nilai factorial 
+print(math.factorial(5))
+
+# mencari nilai akar kuadrat
+print(math.sqrt(16)) 
+
+# ceil untuk mengembalikan nilai integer namun akan di bulatkan ke atas 
+print(math.ceil(16.1)) # akan mengembalikan 17
+
+# floor untuk mengembalikan nilai integer namun akan di bulatkan ke bawah 
+print(math.floor(16.9)) # akan mengembalikan 16
+
+# mengembalikan nilai absolut 
+print(math.fabs(16.9)) # akan mengembalikan 16
+
+
+
